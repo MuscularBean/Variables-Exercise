cf7f2ff1b7c9e7327fe29b6a1403dc3de51e9102
MuscuarBean <email@example.com>
2023-07-14 10:26:15 -0700

    Declared and Initialized variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Jacob";
+            int age = 20;
+            char lastInitial = 'B';
+            bool result = 20>10;
+            decimal courseProg = 12.80m;
+            double courseProgress = 12.8;
+
+            
+
+            Console.WriteLine($"The user's name is {name} {lastInitial}, he is {age} years old. ");
+            Console.WriteLine(result);
+            Console.WriteLine(courseProgress);
+            Console.WriteLine(courseProg);
+
+
+
+            
         }
     }
 }
